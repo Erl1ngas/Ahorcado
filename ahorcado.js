75fe028f66ecfa0381fe6031ec93583c195cc5b5
German <germanigonzalez@gmail.com>
2022-02-05 11:43:11 -0300

    logica de programacion terminada

--- ahorcado.js
+++ ahorcado.js
@@ -1,31 +1,80 @@
 var palabras = [];
 
 var botonIniciarJuego = document.querySelector("#iniciar-juego");
-botonIniciarJuego = addEventListener("click",ejecutarTodo);
+botonIniciarJuego.addEventListener("click", ejecutarTodo);
 
 var botonAgregarPalabra = document.querySelector("#nueva-palabra");
-botonAgregarPalabra.addEventListener("click",AgregarPalabras);
+botonAgregarPalabra.addEventListener("click", AgregarPalabras);
 
 var palabraDesifrar = "";
+var letrasEscritas = [];
+var letrasDescubiertas = [];
 
-function ejecutarTodo(){
+
+function casoIncluyeCaracter(elCaracter) {
+    console.log("disparar dibujito de letra descubierta");
+    letrasDescubiertas.push(elCaracter);
+
+    for (var i = 0; i < palabraDesifrar.length; i++) {
+        const element = palabraDesifrar[i];
+        if (!letrasDescubiertas.includes(element)) {
+            console.log("todavia no ganaste, no encontraste la letra " + element);
+            return;
+        }
+    }
+    console.log("Ganaste el juego");
+    document.onkeyup = function () { }
+
+}
+
+function casoNoIncluyeCaracter() {
+    console.log("disparar dibujo de ahorcado");
+    if ((letrasEscritas.length - letrasDescubiertas.length) > 5) {
+        console.log("Fin del juego");
+        document.onkeyup = function () { }
+    }
+}
+
+function seguimientoDeTipeo(evObject) {
+
+    var elCaracter = String.fromCharCode(evObject.which);
+    if (!(elCaracter >= "A" && elCaracter <= "Z")) {
+        return
+    }
+
+    if (letrasEscritas.includes(elCaracter)) {
+        return
+    }
+
+    letrasEscritas.push(elCaracter);
+    document.querySelector("#letras-probadas").innerHTML = letrasEscritas;
+    console.log(letrasEscritas);
+
+    if (palabraDesifrar.includes(elCaracter)) {
+        casoIncluyeCaracter(elCaracter)
+    } else {
+        casoNoIncluyeCaracter()
+    }
+}
+
+function ejecutarTodo() {
 
     dibujoComienzoJuego();
     Sorteo();
+    letrasDePalabra(palabraDesifrar);
+    document.onkeyup = seguimientoDeTipeo;
 
 }
 
-function Sorteo(){
-        
-        var aleatorio = Math.floor(Math.random()*palabras.length);
-        palabraDesifrar = palabras[aleatorio];
-        console.log(palabraDesifrar);
-    
+function Sorteo() {
+
+    var aleatorio = Math.floor(Math.random() * palabras.length);
+    palabraDesifrar = palabras[aleatorio].toUpperCase();
+    console.log(palabraDesifrar);
 }
 
-function AgregarPalabras(){
+function AgregarPalabras() {
 
     palabras.push(document.querySelector("#input-nueva-palabra").value);
-    console.log(palabras);
 
 }
--- canvas.js
+++ canvas.js
@@ -1,20 +1,23 @@
+var pantalla = document.querySelector("#ahorcado");
+var pincel = pantalla.getContext("2d");
 
-
-function dibujoComienzoJuego(){
-	
-	var pantalla = document.querySelector("#ahorcado");
-	var pincel = pantalla.getContext("2d");
-	pincel.moveTo(160,350);
-	pincel.lineTo(0,400);
-	pincel.lineTo(320,400);
-	pincel.lineTo(160,350);
+function dibujoComienzoJuego() {
+	pincel.moveTo(160, 350);
+	pincel.lineTo(0, 400);
+	pincel.lineTo(320, 400);
+	pincel.lineTo(160, 350);
 	pincel.stroke();
 }
 
-function letrasDePalabra(){
-
-
-
-
+function letrasDePalabra(palabra) {
+	var x = 320;
+	var y = 400;
+	for (i = 0; i < palabra.length; i++) {
+		x = x + 20;
+		pincel.moveTo(x, y);
+		x = x + 20;
+		pincel.lineTo(x, y);
+	}
+	pincel.stroke();
 }
-	
+
